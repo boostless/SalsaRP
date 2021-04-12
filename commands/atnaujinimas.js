a4ac5fa31dd2cfac0665cd0c237d799c7d0aafa7
boostless <46201063+boostless@users.noreply.github.com>
2021-04-12 19:20:27 +0300

    Update atnaujinimas.js

--- commands/atnaujinimas.js
+++ commands/atnaujinimas.js
@@ -4,7 +4,10 @@ module.exports={
     name: 'atnaujinimas',
     description: 'serverio atnaujinimas',
     execute(message, args, bot){
-	if(message.channle.id != '816988698930577409') message.delete() return;
+	if(message.channle.id != '816988698930577409'){
+		message.delete()
+	  return;
+	} 
         message.delete()
         var title = ""
         var description = ""
